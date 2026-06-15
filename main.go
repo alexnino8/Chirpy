a9c6ce6a6a9483b757edab1605fa23b84d77e06f
alexnino8 <alopeznino98@gmail.com>
2026-06-14 18:33:33 -0600

    add readiness endpoint with HandleFunc()

--- main.go
+++ main.go
@@ -8,7 +8,16 @@ import (
 func main() {
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
+
+	//
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
 	server := &http.Server{
 		Addr:    ":8080",
